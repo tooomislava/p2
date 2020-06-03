3042c2a4c12210d67b292bf51f614ae057407ae4
Tomislava Žilić <66275367+tooomislava@users.noreply.github.com>
2020-06-03 22:41:07 +0200

    Add files via upload

---
+++ šablona/src/main/java/ffos/kolokvij0/Start.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ffos.kolokvij0;
+
+import ffos.kolokvij0.utility.HibernateUtil;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        HibernateUtil.getSession();
+    }
+    
+}
---
+++ šablona/src/main/java/ffos/kolokvij0/utility/HibernateUtil.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ffos.kolokvij0.utility;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ *
+ * @author Korisnik
+ */
+// singleton
+public class HibernateUtil {
+
+    private static Session session;
+
+    public static Session getSession() {
+        if (session == null) {
+            try {
+// Create registry
+                StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+// Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
+// Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
+// Create SessionFactory
+                SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+                session = sessionFactory.openSession();
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+        return session;
+    }
+}
